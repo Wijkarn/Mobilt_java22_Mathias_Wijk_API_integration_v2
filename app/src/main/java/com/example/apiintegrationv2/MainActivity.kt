5f52868b2f17ece55ec36b0e89c016221edc4ef5
Wijkarn <mathias.wijk@hotmail.com>
2023-09-14 14:59:40 +0200

    Fixed notifications

--- app/src/main/java/com/example/apiintegrationv2/MainActivity.kt
+++ app/src/main/java/com/example/apiintegrationv2/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.apiintegrationv2
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.FragmentManager
 import com.android.volley.Request
 import com.android.volley.RequestQueue
@@ -80,17 +86,48 @@ class MainActivity : AppCompatActivity() {
 
     private fun showImage(url: String) {
         Log.d("showImage", url)
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        Glide.with(this).load(url).into(imageView)
+
+        Glide.with(this).load(url).into(findViewById<ImageView>(R.id.imageView))
+
+        if (url.takeLast(3) == "gif") notification()
     }
 
     private fun pop() {
         fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         super.onBackPressed()
         // MUST BE IN THIS ORDER
         pop()
+    }
+
+    private fun notification() {
+        val channelId = "APIV2_ID"
+        val notificationId = 1 // A unique ID for the notification
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Wijkarn_Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel = NotificationChannel(channelId, channelName, importance)
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("API V2")
+            .setContentText("Rare item unlocked! You got a gif instead of a image.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        notificationBuilder.setContentIntent(pendingIntent)
+
+        val notificationManager = this.getSystemService(NotificationManager::class.java)
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
--- app/src/main/java/com/example/apiintegrationv2/Warning3.kt
+++ app/src/main/java/com/example/apiintegrationv2/Warning3.kt
@@ -1,16 +1,10 @@
 package com.example.apiintegrationv2
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
@@ -24,7 +18,6 @@ class Warning3 : Fragment() {
         val view = inflater.inflate(R.layout.fragment_warning3, container, false)
 
         view.findViewById<Button>(R.id.yesBtn)?.setOnClickListener {
-            notification()
             val fm = activity?.supportFragmentManager
             val transaction = fm?.beginTransaction()
             transaction?.replace(R.id.frameLayout, Warning1())
@@ -38,32 +31,5 @@ class Warning3 : Fragment() {
         }
 
         return view
-    }
-
-    private fun notification() {
-        val channelId = "APIV2_ID"
-        val notificationId = 1 // A unique ID for the notification
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelName = "Wijkarn_Channel"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val notificationChannel = NotificationChannel(channelId, channelName, importance)
-
-            val notificationManager = requireContext().getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-
-        val notificationBuilder = NotificationCompat.Builder(requireContext(), channelId)
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle("API V2")
-            .setContentText("Sussy boi")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        val intent = Intent(requireContext(), MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        notificationBuilder.setContentIntent(pendingIntent)
-
-        val notificationManager = requireContext().getSystemService(NotificationManager::class.java)
-        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
